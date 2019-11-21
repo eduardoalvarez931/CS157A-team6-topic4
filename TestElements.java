b2ca9998a908d75df4f7fd99a6fecceca7e76b7d
eduardoalvarez931 <43862091+eduardoalvarez931@users.noreply.github.com>
2019-11-20 17:06:12 -0800

    Create TestElements.java

---
+++ TestElements.java
@@ -0,0 +1,286 @@
+package teamproj;
+
+public class TestElements {
+	
+	public TestElements(){}
+	
+	private boolean f25;
+	private boolean f50;
+	private boolean f1;
+	private boolean f2;
+	private boolean f3;
+	private boolean f4;
+	private boolean f6;
+	private boolean f8;
+	private boolean f10;
+	private boolean f12;
+	
+	private int tinPitchMatch;//tinnitus pitch match
+	private int tinMatchType;//tinnitus match type
+	private int tinLoudMatch;//tinnitus loudness match
+	private int thresHearing;//threshold of hearing (Th)
+	private int thresHearTLs;//threshold of hearing (TLs)
+	
+	private int WN;
+	
+	//minimal masking level
+	private int mr;
+	private int m_b;
+	private int mb_;
+	
+	private int sd;
+	
+	private int l50;
+	private int l1;
+	private int l2;
+	private int l3;
+	private int l4;
+	private int l6;
+	private int l8;
+	private int l12;
+	private int ltp;
+	
+	private String comments;
+
+	public boolean getF25() {
+		return f25;
+	}
+
+	public boolean getF50() {
+		return f50;
+	}
+
+	public boolean getF1() {
+		return f1;
+	}
+
+	public boolean getF2() {
+		return f2;
+	}
+
+	public boolean getF3() {
+		return f3;
+	}
+
+	public boolean getF4() {
+		return f4;
+	}
+
+	public boolean getF6() {
+		return f6;
+	}
+
+	public boolean getF8() {
+		return f8;
+	}
+
+	public boolean getF10() {
+		return f10;
+	}
+
+	public boolean getF12() {
+		return f12;
+	}
+
+	public int getTinPitchMatch() {
+		return tinPitchMatch;
+	}
+
+	public int getTinMatchType() {
+		return tinMatchType;
+	}
+
+	public int getTinLoudMatch() {
+		return tinLoudMatch;
+	}
+
+	public int getThresHearing() {
+		return thresHearing;
+	}
+
+	public int getThresHearTLs() {
+		return thresHearTLs;
+	}
+
+	public int getWN() {
+		return WN;
+	}
+
+	public int getMr() {
+		return mr;
+	}
+
+	public int getM_b() {
+		return m_b;
+	}
+
+	public int getMb_() {
+		return mb_;
+	}
+
+	public int getSd() {
+		return sd;
+	}
+
+	public int getL50() {
+		return l50;
+	}
+
+	public int getL1() {
+		return l1;
+	}
+
+	public int getL2() {
+		return l2;
+	}
+
+	public int getL3() {
+		return l3;
+	}
+
+	public int getL4() {
+		return l4;
+	}
+
+	public int getL6() {
+		return l6;
+	}
+
+	public int getL8() {
+		return l8;
+	}
+
+	public int getL12() {
+		return l12;
+	}
+
+	public int getLtp() {
+		return ltp;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setF25(boolean f25) {
+		this.f25 = f25;
+	}
+
+	public void setF50(boolean f50) {
+		this.f50 = f50;
+	}
+
+	public void setF1(boolean f1) {
+		this.f1 = f1;
+	}
+
+	public void setF2(boolean f2) {
+		this.f2 = f2;
+	}
+
+	public void setF3(boolean f3) {
+		this.f3 = f3;
+	}
+
+	public void setF4(boolean f4) {
+		this.f4 = f4;
+	}
+
+	public void setF6(boolean f6) {
+		this.f6 = f6;
+	}
+
+	public void setF8(boolean f8) {
+		this.f8 = f8;
+	}
+
+	public void setF10(boolean f10) {
+		this.f10 = f10;
+	}
+
+	public void setF12(boolean f12) {
+		this.f12 = f12;
+	}
+
+	public void setTinPitchMatch(int tinPitchMatch) {
+		this.tinPitchMatch = tinPitchMatch;
+	}
+
+	public void setTinMatchType(int tinMatchType) {
+		this.tinMatchType = tinMatchType;
+	}
+
+	public void setTinLoudMatch(int tinLoudMatch) {
+		this.tinLoudMatch = tinLoudMatch;
+	}
+
+	public void setThresHearing(int thresHearing) {
+		this.thresHearing = thresHearing;
+	}
+
+	public void setThresHearTLs(int thresHearTLs) {
+		this.thresHearTLs = thresHearTLs;
+	}
+
+	public void setWN(int wN) {
+		WN = wN;
+	}
+
+	public void setMr(int mr) {
+		this.mr = mr;
+	}
+
+	public void setM_b(int m_b) {
+		this.m_b = m_b;
+	}
+
+	public void setMb_(int mb_) {
+		this.mb_ = mb_;
+	}
+
+	public void setSd(int sd) {
+		this.sd = sd;
+	}
+
+	public void setL50(int l50) {
+		this.l50 = l50;
+	}
+
+	public void setL1(int l1) {
+		this.l1 = l1;
+	}
+
+	public void setL2(int l2) {
+		this.l2 = l2;
+	}
+
+	public void setL3(int l3) {
+		this.l3 = l3;
+	}
+
+	public void setL4(int l4) {
+		this.l4 = l4;
+	}
+
+	public void setL6(int l6) {
+		this.l6 = l6;
+	}
+
+	public void setL8(int l8) {
+		this.l8 = l8;
+	}
+
+	public void setL12(int l12) {
+		this.l12 = l12;
+	}
+
+	public void setLtp(int ltp) {
+		this.ltp = ltp;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	
+	
+}
