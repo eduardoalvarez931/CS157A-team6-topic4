3b261b3686208c7b45dd45c6eb854460a54922ba
Diego Leiva <33105822+dLeiva376@users.noreply.github.com>
2019-11-25 18:02:37 -0800

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -10,4 +10,43 @@ public class Patient {
 	{
 		
 	}
+	
+	public Patient(String firstName, String lastName, String thcNumber, Visit visit) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.thcNumber = thcNumber;
+		this.visit = visit;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getThcNumber() {
+		return thcNumber;
+	}
+
+	public void setThcNumber(String thcNumber) {
+		this.thcNumber = thcNumber;
+	}
+
+	public Visit getVisit() {
+		return visit;
+	}
+
+	public void setVisit(Visit visit) {
+		this.visit = visit;
+	}
 }
