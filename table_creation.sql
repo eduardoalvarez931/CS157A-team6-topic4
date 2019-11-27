01293006a797590151ac8f508ee3b58ce170971e
Brielle Purnell <39754055+BriellePurnell@users.noreply.github.com>
2019-11-27 13:58:10 -0800

    added more attributes to medication table

--- table_creation.sql
+++ table_creation.sql
@@ -113,7 +113,11 @@ CREATE TABLE medication (
     disease_id					   SMALLINT NOT NULL,
     medication_name                VARCHAR(25) NOT NULL,
     medication_description         VARCHAR(256) NOT NULL,
-    usual_dose                     VARCHAR(45) NOT NULL
+    usual_dose                     VARCHAR(45) NOT NULL,
+    medication_action		   VARCHAR(48) NOT NULL,
+    application			   VARCHAR(128) NOT NULL,
+    t_side			   BOOLEAN NOT NULL,
+    med_comments		   VARCHAR(256)
 );
 ALTER TABLE medication ADD CONSTRAINT medication_pk PRIMARY KEY ( medication_id );
 ALTER TABLE medication ADD CONSTRAINT disease_fk FOREIGN KEY ( disease_id ) REFERENCES disease (disease_id);
